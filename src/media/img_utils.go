d0d7f6872d3d1b3dbe37c9c69ab9588183d00395
howiechen <howiechen95@gmail.com>
2021-07-19 00:13:07 +0800

    +test

--- src/media/img_utils.go
+++ src/media/img_utils.go
@@ -1,13 +1,17 @@
 package media
 
 import (
+	"fmt"
 	"github.com/nfnt/resize"
+	"image"
 	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
+	"time"
 )
 
-func ResizeImage(fileUri string, width, height uint) {
+func ResizeJpg(fileUri string, width, height uint) {
 	// open "test.jpg"
 	file, err := os.Open(fileUri)
 	if err != nil {
@@ -25,7 +29,7 @@ func ResizeImage(fileUri string, width, height uint) {
 	// and preserve aspect ratio
 	m := resize.Resize(width, height, img, resize.Lanczos3)
 
-	out, err := os.Create("test_resized.jpg")
+	out, err := os.Create(fmt.Sprintf("test_resized_%d.jpg", time.Now().Unix()))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,4 +37,67 @@ func ResizeImage(fileUri string, width, height uint) {
 
 	// write new image to file
 	jpeg.Encode(out, m, nil)
+}
+
+func ResizePng(fileUri string, width, height uint) {
+	// open "test.jpg"
+	file, err := os.Open(fileUri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode jpeg into image.Image
+	img, err := png.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	// resize to width 1000 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(width, height, img, resize.Lanczos3)
+
+	out, err := os.Create(fmt.Sprintf("test_resized_%d.png", time.Now().Unix()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	png.Encode(out, m)
+}
+
+func ResizeImage1(fileUri string, width, height uint) {
+	// open "test.jpg"
+	file, err := os.Open(fileUri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, imgType, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	file.Close()
+
+	// resize to width 1000 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(width, height, img, resize.Lanczos3)
+
+	out, err := os.Create(fmt.Sprintf("test_resized_%d.jpg", time.Now().Unix()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	jpeg.Encode(out, m, nil)
+	if imgType == "jpg" || imgType == "jpeg" {
+		jpeg.Encode(out, img, nil)
+	} else if imgType == "png" {
+		png.Encode(out, img)
+	} else {
+		log.Fatal("not match type")
+	}
 }
--- src/media/img_utils_test.go
+++ src/media/img_utils_test.go
@@ -4,9 +4,14 @@ import (
 	"testing"
 )
 
-func TestResizeImage(t *testing.T) {
-	fileUri := "C:\\Users\\Administrator\\Pictures\\86446038_p0.jpg"
-	//fileUri := "C:\\Users\\Administrator\\Pictures\\Snipaste_2021-06-12_08-40-44.png"
+func TestResizePng(t *testing.T) {
+	fileUri := "./test01.png"
 	width, height := uint(400), uint(400)
-	ResizeImage(fileUri, width, height)
+	ResizePng(fileUri, width, height)
+}
+
+func TestResizeJpg(t *testing.T) {
+	fileUri := "./test02.jpg"
+	width, height := uint(400), uint(400)
+	ResizeJpg(fileUri, width, height)
 }
